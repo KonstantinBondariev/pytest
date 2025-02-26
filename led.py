c2b2c79adc0ac84e50d31b145106bfee45036ad9
Konstantin Bondariev <bondariev.konstantin@gmail.com>
2025-02-26 23:08:47 +0200

    Implement button control for LED state and remove logging of pin states

--- led.py
+++ led.py
@@ -2,36 +2,27 @@ import RPi.GPIO as GPIO
 import time
 
 LED_PIN = 17  # GPIO pin where the LED is connected
+BTN_PIN = 22  # GPIO pin where the button is connected
 
 GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
 GPIO.setup(LED_PIN, GPIO.OUT)  # Set pin as an output
-
-def log_pin_states():
-  for pin in range(2, 28):  # GPIO pins range from 2 to 27
-    try:
-      state = GPIO.input(pin)
-      print(f"Pin {pin}: {'HIGH' if state else 'LOW'}")
-    except RuntimeError:
-      print(f"Pin {pin}: Not available")
+GPIO.setup(BTN_PIN, GPIO.IN)  # Set pin as an input
 
 
 try:
   while True:
-    log_pin_states()
+   
+    if GPIO.input(BTN_PIN) == GPIO.HIGH:
+      GPIO.output(LED_PIN, GPIO.HIGH)  # Turn on
+    else:
+      GPIO.output(LED_PIN, GPIO.LOW)  # Turn off
+    time.sleep(0.1)  # Wait for a short time
 
-    GPIO.output(LED_PIN, GPIO.HIGH)  # Turn on LED
-    log_pin_states()
-    time.sleep(10)  # Wait for 1 second
-
-    GPIO.output(LED_PIN, GPIO.LOW)  # Turn off LED
-    log_pin_states()
-    time.sleep(10)  # Wait for 1 second
 except KeyboardInterrupt:
   pass
 finally:
   print("Cleaning up")
   GPIO.cleanup()  # Clean up GPIO to reset the pin state
-  log_pin_states()
 
 
 
